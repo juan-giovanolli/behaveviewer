f896613106d31fabba3e273999510969eb169530
Juan Giovanolli <juangiovanolli@yahoo.com.ar>
2016-05-26 10:43:20 -0300

    CGanges in model and added service

--- model/Scenario.py
+++ model/Scenario.py
@@ -3,7 +3,8 @@ Created on 24 de may. de 2016
 
 @author: Juan
 '''
-from peewee import SqliteDatabase, CharField, Model, ForeignKeyField
+from peewee import SqliteDatabase, CharField, Model, ForeignKeyField,\
+    BooleanField
 from Feature import Feature
 from Tag import Tag
 from playhouse.fields import ManyToManyField
@@ -18,7 +19,8 @@ class Scenario(Model):
     name = CharField()
     description = CharField(null=True)
     feature = ForeignKeyField(Feature, related_name='scenarios')
-    ##tags = ManyToManyField(Tag, related_name='scenarios')
+    is_background = BooleanField()
+    tags = ManyToManyField(Tag, related_name='scenarios')
     
     class Meta:
         database = db
