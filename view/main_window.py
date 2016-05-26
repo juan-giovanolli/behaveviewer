d5251b77c09a2084dae4482d8d28931773bbb4e9
RicardoRodriguezArg <aricardorodriguez@hotmail.com>
2016-05-26 09:44:51 -0300

    creacion Thread para manejo procesos en background

--- view/main_window.py
+++ view/main_window.py
@@ -1,7 +1,9 @@
 import sys
+import time
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import QThread, SIGNAL
 from tables_content_manager import TableDataRepresentation
-
+from service_worker_thread import ServiceThread
 
 class MainBehaveWindow(QtGui.QTabWidget):
 
@@ -12,9 +14,13 @@ class MainBehaveWindow(QtGui.QTabWidget):
     __TAB_FEATURE_INDEX = 1
     __TAB_STEP_INDEX = 2
     __TAB_STATISTICS_INDEX = 3
+    #TODO: change this to make this code multi-platform
+    __CURRENT_PATH_LINUX = "."
+    __EMPTY_TEXT = ""
+    __APP_NAME = 'Behave Links'
     __MAIN_LABEL_TEXT = "Features Directory: "
     __TABLE_CONFIG = {"steps_table":{"table_column_titles":"name, descripcion, scenario, code_step"},
-                      "feature_table":{"table_column_titles":"name, descripcion, scenario, code_step"},
+                      "feature_table":{"table_column_titles":"name, descripcion"},
                       "statistics_table":{"table_column_titles":"name, descripcion, scenario, code_step"}
                       }
 
@@ -22,6 +28,10 @@ class MainBehaveWindow(QtGui.QTabWidget):
         super(MainBehaveWindow, self).__init__()
         self.__is_feature_directory_loaded=False
         self.initUI()
+        self.__create_function_map_for_worker()
+        self.__service_worker = ServiceThread(self._function_dictionary)
+        #conectando funcion a la finalizacion del thread
+        self.connect(self.__service_worker, SIGNAL("finished()"), self._process_terminated)
 
 
     def initUI(self):
@@ -29,7 +39,7 @@ class MainBehaveWindow(QtGui.QTabWidget):
         self.__register_all_tabs()
         self.__lock_unlock_tabs(self.__is_feature_directory_loaded)
         self.setGeometry(self.__WINDOWS_POS_X, self.__WINDOWS_POS_Y, self.__WINDOWS_WEIGHT, self.__WINDOWS_HEIGHT)
-        self.setWindowTitle('Behave Links')
+        self.setWindowTitle(self.__APP_NAME)
         self.show()
 
 
@@ -42,8 +52,10 @@ class MainBehaveWindow(QtGui.QTabWidget):
 
     def __load_feature_directory(self):
         print "loading features directory"
-        self.__is_feature_directory_loaded=True
-        self.__lock_unlock_tabs( self.__is_feature_directory_loaded)
+        #TODO: Este codigo esta en verificacion
+        #self.__is_feature_directory_loaded=True
+        #self.__lock_unlock_tabs( self.__is_feature_directory_loaded)
+        self.__show_dialog_find_files()
 
     def __lock_unlock_tabs(self, lock_status):
         self.setTabEnabled(self.__TAB_FEATURE_INDEX, lock_status )
@@ -67,10 +79,12 @@ class MainBehaveWindow(QtGui.QTabWidget):
         boton_config= {"X_POS":"300","Y_POS":"70"}
         horizontal_layout.addWidget(self.__create_botton("Load Feature Directory: ", self.__load_feature_directory,boton_config))
         horizontal_layout.addStretch(1)
-
         self.main_tab_layout.addLayout(horizontal_layout)
         boton_config["X_POS"]="300"
         boton_config["Y_POS"]="50"
+        self.__process_label_verbose=self.__create_label(self.__EMPTY_TEXT)
+        self.main_tab_layout.addWidget(self.__process_label_verbose)
+        self.main_tab_layout.addStretch(1)
         self.main_tab_layout.addWidget(self.__create_botton("Quit",QtCore.QCoreApplication.instance().quit,boton_config))
         self.__main_tab.setLayout(self.main_tab_layout)
 
@@ -85,24 +99,81 @@ class MainBehaveWindow(QtGui.QTabWidget):
     def __create_features_tab(self):
         self.__feature_tab = QtGui.QWidget()
         features_tab_layout = QtGui.QVBoxLayout()
-        features_tab_layout.addWidget(TableDataRepresentation(None, self.__TABLE_CONFIG["steps_table"]))
+        features_tab_layout.addWidget(TableDataRepresentation(None, self.__TABLE_CONFIG["feature_table"]))
         self.__feature_tab.setLayout(features_tab_layout)
 
     def __create_steps_tab(self):
         self.__steps_tab = QtGui.QWidget()
+        step_tab_layout = QtGui.QVBoxLayout()
+        step_tab_layout.addWidget(TableDataRepresentation(None, self.__TABLE_CONFIG["steps_table"]))
+        self.__steps_tab.setLayout(step_tab_layout)
 
 
     def __create_statistics_tab(self):
         self.__statistics_tab = QtGui.QWidget()
-
+        statistics_tab_layout = QtGui.QVBoxLayout()
+        statistics_tab_layout.addWidget(TableDataRepresentation(None, self.__TABLE_CONFIG["statistics_table"]))
+        self.__statistics_tab.setLayout(statistics_tab_layout)
 
     def __register_tab(self, tab, tab_label):
         self.addTab(tab, tab_label)
 
 
     def __show_dialog_find_files(self):
-        fileName = QtGui.QFileDialog.getOpenFileName(self, 'OpenFile')
-        self.myTextBox.setText(fileName)
+        options = QtGui.QFileDialog.DontResolveSymlinks | QtGui.QFileDialog.ShowDirsOnly
+        #TODO: change this to make this code multi-platform
+        feature_directory_path = None
+        feature_directory_path = QtGui.QFileDialog.getExistingDirectory(self, 'SelectDirectory',\
+                                                                self.__CURRENT_PATH_LINUX,\
+                                                                options)
+        #TODO: Este codigo esta en testing
+        self.__service_worker.start()
+        #time.sleep(2)
+        #self.parsing_directory(feature_directory_path)
+        #self.creating_db_tables()
+        #self.fill_view_tables_with_sql()
+
+
+    def parsing_directory(self,feature_directory_path = None):
+        #TODO: Crear variable que setea estado de la aplicacion
+        if feature_directory_path is not None:
+            self.setTextInVerboseLabel("Seletcted Path : {0}".format(feature_directory_path))
+            time.sleep(1)
+            print "Path Seleccionado: {0}".format(feature_directory_path)
+            self.setTextInVerboseLabel("parsing directory ....")
+            time.sleep(1)
+            print "parsing directory ...."
+
+    def creating_db_tables(self):
+        self.setTextInVerboseLabel("create DataBase tables .....")
+        time.sleep(1)
+        print "create DataBase tables ....."
+
+    def fill_view_tables_with_sql(self):
+        print "Filling View tables from SQL Sentences........."
+        self.setTextInVerboseLabel("Filling View tables from SQL Sentences.........")
+        time.sleep(1)
+        self.__fill_steps_table()
+        self.__fill_feature_table()
+        self.__fill_statistics_table()
+
+    def __fill_steps_table(self):
+        self.setTextInVerboseLabel("fill_steps_table")
+        time.sleep(1)
+        print "fill_steps_table"
+
+
+    def __fill_feature_table(self):
+        self.setTextInVerboseLabel("__fill_feature_table")
+        time.sleep(1)
+        print "__fill_feature_table"
+
+
+    def __fill_statistics_table(self):
+        self.setTextInVerboseLabel("__fill_statistics_table")
+        time.sleep(1)
+        print "__fill_statistics_table"
+
 
     def __create_tabs(self):
         self.__create_main_tab()
@@ -110,6 +181,19 @@ class MainBehaveWindow(QtGui.QTabWidget):
         self.__create_steps_tab()
         self.__create_statistics_tab()
 
+    def __create_function_map_for_worker(self):
+        self._function_dictionary = {"PARSING_DIRECTORY: ":self.parsing_directory,\
+                                     "CREATING_DB_TABLES:":self.creating_db_tables,\
+                                     "FILL_VIEW_TABLE: ":self.fill_view_tables_with_sql
+                                     }
+    def _process_terminated(self):
+        self.setTextInVerboseLabel("Process Terminated")
+        self.__lock_unlock_tabs( True)
+
+
+    def setTextInVerboseLabel(self, label_text):
+        self.__process_label_verbose.clear()
+        self.__process_label_verbose.setText(label_text)
 
 def main():
     app = QtGui.QApplication(sys.argv)
---
+++ view/service_worker_thread.py
@@ -0,0 +1,26 @@
+from PyQt4.QtCore import QThread, SIGNAL
+
+
+class ServiceThread(QThread):
+    def __init__(self, function_map):
+        QThread.__init__(self)
+        self._function_map= function_map
+
+    def __del__(self):
+        self.wait()
+
+    def set_reference_path(self, reference_path):
+        self._reference_path=reference_path
+    def run(self):
+        print "Ejecutando Hilo"
+        if self._function_map is not None:
+            _process_worker()
+
+
+    def _process_worker():
+        for key, values in self._function_map.iteritems():
+            if key == "PARSING_DIRECTORY":
+                values(self._reference_path)
+            else:
+                values()
+            self.emit(SIGNAL('setTextInVerboseLabel(QString)'), key)
