bbd53fc48627a87b85b38de20dfcfdffc01f49b1
Federico Ferri <federicoferriff@gmail.com>
2016-06-02 12:36:52 -0300

    pep8 on main.py

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ Created on 24 de may. de 2016
 @author: Juan
 '''
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 from config.setup import Setup
 from service.entity_service import EntityService
 from view.main_window import MainBehaveWindow
 
+
 def main():
-    Setup({'reset_db':True, 'dummy_db':False})
+    Setup({'reset_db': True, 'dummy_db': False})
 
     app = QtGui.QApplication(sys.argv)
-    ex = MainBehaveWindow(EntityService())
+    MainBehaveWindow(EntityService())
     sys.exit(app.exec_())
-    
+
+
 if __name__ == '__main__':
     main()
