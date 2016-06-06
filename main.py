58e72442b08b0795cf39308bc8e7669d4fbfa18a
Federico Ferri <federicoferriff@gmail.com>
2016-06-06 14:47:49 -0300

    csv file created correctly

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ parsing_directory = ''
 
 
 def main(ui_flag, parsing_directory=None):
-    Setup({'reset_db': True, 'dummy_db': False})
+    Setup({'reset_db': False, 'dummy_db': False})
     if ui_flag:
         main_no_ui(parsing_directory)
     else:
@@ -20,12 +20,12 @@ def main(ui_flag, parsing_directory=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--dir', nargs=1)
     parser.add_argument('--no-ui', action='store_true',
-                        help='Runs parser without UI and prints on stdout')
-    parser.add_argument('-d', '--dir', nargs=1, action='store_const', default=None,
-                        const=parsing_directory)
+                        help='Runs parser without UI and creates the csv file')
 
     import sys
     print "HOLA        ", sys.argv
     args = parser.parse_args()
-    main(args.no_ui, parsing_directory)
+    print args.dir
+    main(args.no_ui, args.dir[0])
--- main_no_ui.py
+++ main_no_ui.py
@@ -1,25 +1,46 @@
 import os
+import csv
 from config.setup import db
 from gherkin_parser.parser_helper import ParserHelper
 from gherkin_parser.code_parser import CodeParser
+from service.entity_service import EntityService
 
 
 class NoUiRun:
     def __init__(self):
-        pass
+        self.db_manager = EntityService()
 
     def parsing_directory(self, feature_directory_path=None):
         assert feature_directory_path is not None
-        if os.path.isfile():
+        print feature_directory_path
+        if os.path.isdir(feature_directory_path):
             feature_directory_path = unicode(feature_directory_path)
             path_to_step = os.path.join(feature_directory_path, "steps")
             db.begin()
-            CodeParser().parseDir(path_to_step)
-            ParserHelper(feature_directory_path)
-            db.commit()
             print "parsing directory ...."
+            # CodeParser().parseDir(path_to_step)
+            # ParserHelper(feature_directory_path)
+            db.commit()
+        else:
+            print "File doesn't exist"
+
+    def generate_csv_files(self):
+        with open('steps.csv', 'wb') as csvfile:
+            row_writer = csv.writer(csvfile, delimiter=';',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            row_writer.writerow(['Feature', 'Scenario', 'Step', 'Code Step', 'Tags'])
+            for feature in self.db_manager.find_features(""):
+                for step in self.db_manager.find_steps_per_feature(feature.id):
+                    code_step = self.db_manager.find_code_step(step.name)
+                    if not code_step:
+                        code_step = ''
+                    for tag in step.scenario.tags:
+                        new_row = [feature.name, step.scenario.name, step.name, code_step, tag.name]
+                        print new_row
+                        row_writer.writerow(new_row)
 
 
 def main_no_ui(feature_directory_path=None):
     assert feature_directory_path, "No feature path given"
     NoUiRun().parsing_directory(feature_directory_path)
+    NoUiRun().generate_csv_files()
